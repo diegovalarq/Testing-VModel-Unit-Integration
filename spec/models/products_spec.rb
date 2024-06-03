8efc1c034361acb2488fa70d30d503208247bdec
benjarsanchez <80903293+benjarsanchez@users.noreply.github.com>
2024-06-03 17:34:17 -0400

    feat(Unit Test): product model

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -12,4 +12,63 @@ RSpec.describe Product, type: :model do
   it 'is valid with valid attributes' do
     expect(@product).to be_valid
   end
+
+  it 'is invalid without a category' do
+    @product.categories = nil
+    expect(@product).to_not be_valid
+  end
+
+  it 'is invalid with multiple categories' do
+    @product.categories = 'Cancha, Accesorio tecnologico'
+    expect(@product).to_not be_valid
+  end
+
+  it 'is invalid with empty a name' do
+    @product.nombre = nil
+    expect(@product).to_not be_valid
+  end
+
+  it 'is invalid with empty a stock' do
+    @product.stock = nil
+    expect(@product).to_not be_valid
+  end
+
+  it 'is valid with stock greater than 0' do
+    @product.stock = 100
+    expect(@product).to be_valid
+  end
+
+  it 'is valid with stock equal to 0' do
+    @product.stock = 0
+    expect(@product).to be_valid
+  end
+
+  it 'is invalid with stock less than 0' do
+    @product.stock = -1
+    expect(@product).to_not be_valid
+  end
+
+  it 'is invalid with empty a price' do
+    @product.precio = nil
+    expect(@product).to_not be_valid
+  end
+
+  it 'is valid with price greater than 0' do
+    @product.precio = 100
+    expect(@product).to be_valid
+  end
+
+  it 'is valid with price equal to 0' do
+    @product.precio = 0
+    expect(@product).to be_valid
+  end
+
+  it 'is invalid with price less than 0' do
+    @product.precio = -1
+    expect(@product).to_not be_valid
+  end
+
+
+
+
 end
