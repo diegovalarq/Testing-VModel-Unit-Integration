798d3e6728a3fc48bcc323249ac6de80d3e4ecba
benjarsanchez <80903293+benjarsanchez@users.noreply.github.com>
2024-06-04 20:20:14 -0400

    change_test(product model): change product name & rm admin role)

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -4,9 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   before(:each) do
-    @user = User.create!(name: 'John1', password: 'Nonono123!', email: 'asdf@gmail.com',
-                         role: 'admin')
-    @product = Product.new(nombre: 'John1', precio: 4000, stock: 1, user_id: @user.id, categories: 'Cancha')
+    @user = User.create!(name: 'John1', password: 'Nonono123!', email: 'asdf@gmail.com')
+    @product = Product.new(nombre: 'Pala Babolat', precio: 4000, stock: 1, user_id: @user.id, categories: 'Cancha')
   end
 
   it 'is valid with valid attributes' do
