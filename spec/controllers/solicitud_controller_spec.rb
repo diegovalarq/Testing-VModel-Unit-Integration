a4d3e93cef7ed469c3c13797fdeeec2bb99cb4ab
Diego <diegovalarq@uc.cl>
2024-06-07 16:16:42 -0400

    part 2: change tests type from 'request' to 'controller'

--- spec/controllers/solicitud_controller_spec.rb
+++ spec/controllers/solicitud_controller_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe 'Solicitud', type: :request do
+RSpec.describe SolicitudController, type: :controller do
   before do
-    @user = User.create!(name: 'John', password: 'Password123!', email: 'john@example.com', role: 'user')
-    @product = Product.create!(nombre: 'Product1', precio: 5000, stock: 10, user_id: @user.id, categories: 'Cancha')
-    sign_in @user
+    @admin_user = User.create!(name: 'Admin', password: 'Password123!', email: 'admins@example.com', role: 'admin')
+    @product = Product.create!(nombre: 'Product1', precio: 5000, stock: 5, user_id: @admin_user.id, categories: 'Cancha')
+    sign_in @admin_user
   end
 
-  describe 'GET /solicitud/index' do
+  describe 'GET #index' do
     it 'renders the index template' do
-      get '/solicitud/index'
+      get :index
       expect(response).to render_template(:index)
     end
   end
 
-  describe 'POST /solicitud/insertar' do
+  describe 'POST #insertar' do
     context 'with valid parameters' do
       it 'creates a new solicitud' do
-        solicitud_params = { solicitud: { stock: 5 }, product_id: @product.id }
+        solicitud_params = { solicitud: { stock: 5 }, product_id: 
+        @product.id }
         expect {
-          post '/solicitud/insertar', params: solicitud_params
+          post :insertar, params: solicitud_params
         }.to change(Solicitud, :count).by(1)
         expect(response).to redirect_to("/products/leer/#{@product.id}")
         expect(flash[:notice]).to eq('Solicitud de compra creada correctamente!')
@@ -29,27 +30,28 @@ RSpec.describe 'Solicitud', type: :request do
     context 'with invalid parameters' do
       it 'does not create a new solicitud' do
         solicitud_params = { solicitud: { stock: 15 }, product_id: @product.id }
-        post '/solicitud/insertar', params: solicitud_params
+        post :insertar, params: solicitud_params
         expect(response).to redirect_to("/products/leer/#{@product.id}")
         expect(flash[:error]).to eq('No hay suficiente stock para realizar la solicitud!')
       end
     end
   end
 
-  describe 'DELETE /solicitud/eliminar' do
+  describe 'DELETE #eliminar' do
     it 'deletes an existing solicitud' do
-      solicitud = Solicitud.create!(status: 'Pendiente', stock: 5, product_id: @product.id, user_id: @user.id)
-      delete "/solicitud/eliminar/#{solicitud.id}"
+      solicitud = Solicitud.create!(status: 'Pendiente', stock: 5, product: @product, user: @admin_user)
+      expect {
+        delete :eliminar, params: { id: solicitud.id }
+      }.to change(Solicitud, :count).by(-1)
       expect(response).to redirect_to('/solicitud/index')
       expect(flash[:notice]).to eq('Solicitud eliminada correctamente!')
-      expect(Solicitud.exists?(solicitud.id)).to be_falsey
     end
   end
 
-  describe 'PATCH /solicitud/actualizar' do
+  describe 'PATCH #actualizar' do
     it 'updates the status of a solicitud' do
-      solicitud = Solicitud.create!(status: 'Pendiente', stock: 5, product_id: @product.id, user_id: @user.id)
-      patch "/solicitud/actualizar/#{solicitud.id}"
+      solicitud = Solicitud.create!(status: 'Pendiente', stock: 5, product: @product, user: @admin_user)
+      patch :actualizar, params: { id: solicitud.id }
       expect(response).to redirect_to('/solicitud/index')
       expect(flash[:notice]).to eq('Solicitud aprobada correctamente!')
       expect(solicitud.reload.status).to eq('Aprobada')
